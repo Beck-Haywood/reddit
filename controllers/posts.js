532803c006f2ae2d146a4f601ee7f2db9c4790f5
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-02-03 16:37:08 -0800

    users can see all posts on index

---
+++ controllers/posts.js
@@ -0,0 +1,28 @@
+const Post = require('../models/post');
+
+module.exports = app => {
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+            console.log(err)
+            console.log(post)
+
+            // REDIRECT TO THE ROOT
+            return res.redirect(`/`); 
+        })
+    });
+    app.get("/", (req, res) => {
+        Post.find({})
+        .then(posts => {
+            console.log(posts)
+            res.render("posts-index", {posts});
+        })
+        .catch(err => {
+            console.log('error', err.message);
+        });
+    })
+};
+
--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
+// Use Body Parser
 // Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const exphbs  = require('express-handlebars');
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+
+require('./controllers/posts.js')(app);
+
+// Set db
+require('./data/reddit-db');
+
+// Routes
+
+
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new')
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
-
-// Routes
-app.get('/', (req, res) => {
-    res.render('index')
-  })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
